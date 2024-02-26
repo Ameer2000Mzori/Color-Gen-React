014ab36ae9d1857e05e844396f5012558ee500cd
Ameer Ameen <ameerapex496@gmail.com>
2024-02-26 21:39:35 +0100

    added logic for color

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ColorGenLogic from './hooks/ColorGenLogic.jsx'
 
 const Home = () => {
-  return <ColorGenLogic />
+  const [color, setColor] = useState()
+  return (
+    <>
+      <div>
+        <div>
+          <input
+            type="color"
+            value={color}
+            onChange={(e) => {
+              setColor(e.target.value)
+            }}
+          />
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Home
