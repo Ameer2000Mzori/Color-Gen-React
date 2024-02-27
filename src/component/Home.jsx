3b153f5297e4edc9b2f15030cca6a15fd7437c8f
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-27 10:26:06 +0100

    added logic where you can select color and generate colors

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -3,7 +3,7 @@ import ColorGenLogic from './hooks/ColorGenLogic.jsx'
 
 const Home = () => {
   const [color, setColor] = useState('red')
-
+  const [colors, setColors] = useState([])
   return (
     <>
       <div>
@@ -19,13 +19,31 @@ const Home = () => {
             <button
               onClick={() => {
                 let colorSet = ColorGenLogic(color)
-                console.log(colorSet)
+                setColors(colorSet)
+                console.log(colors)
               }}
             >
               get colors
             </button>
           </div>
           <h1>select a color</h1>
+        </div>
+        <div>
+          <h1>our colors</h1>
+          <div className="flex flex-row flex-wrap gap-4 text-center items-center justify-center">
+            {colors.map((color, index) => {
+              console.log(color, color.rgb[0])
+              return (
+                <div
+                  className="h-[40px] w-[40px]"
+                  style={{
+                    backgroundColor: `rgb(${color.rgb[0]}, ${color.rgb[1]},${color.rgb[2]})`,
+                  }}
+                  key={index}
+                ></div>
+              )
+            })}
+          </div>
         </div>
       </div>
     </>
