8d64d2fe7ececc273c643e5ed3d497b5b0e8a1e5
Ameer Mohammed Ameen <ameerapex496@gmail.com>
2024-02-27 10:58:22 +0100

    added everything that was needed

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -7,9 +7,9 @@ const Home = () => {
   const [colors, setColors] = useState([])
   return (
     <>
-      <div>
-        <div>
-          <div>
+      <div className="flex flex-col text-center items-center justify-center h-[100vh] w-[100vw] p-4">
+        <div className="h-[10%] w-[100%] flex flex-row text-center items-center justify-center gap-4">
+          <div className="flex flex-row text-center items-center justify-center gap-4">
             <input
               type="color"
               value={color}
@@ -28,6 +28,7 @@ const Home = () => {
               max={100}
             />
             <button
+              className="w-[100px] h-[40px] bg-zinc-400 rounded-sm text-white"
               onClick={() => {
                 if (colorsSets > 100 || colorsSets < 0) {
                   alert('min=  1 / max = 100')
@@ -41,11 +42,10 @@ const Home = () => {
               get colors
             </button>
           </div>
-          <h1>select a color</h1>
         </div>
-        <div>
+        <div className="w-[100%] h-[90%] flex flex-col text-center items-center justify-start p-4 m-4">
           <h1>our colors</h1>
-          <div className="flex flex-row flex-wrap gap-4 text-center items-center justify-center">
+          <div className=" w-[60%] flex flex-row flex-wrap gap-4 text-center items-center justify-center">
             {colors.map((color, index) => {
               return (
                 <div
